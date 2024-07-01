f572720c261a2b7881ba2bc0ee71e437d6e68211
Krai53n <greasha46@gmail.com>
2024-07-01 23:06:32 +0300

    add address filling for the server menu

--- xonix/online_coop_menu.py
+++ xonix/online_coop_menu.py
@@ -14,30 +14,112 @@ from fonts import fonts
 from popup_messages import PopupMessage
 
 
-class Server(PopupMessage):
-    def __init__(self, scenes: deque):
-        super().__init__(scenes, 'Waiting user')
-        self.y = 12
+class FillAddr:
+    def __init__(self):
+        self.classes = ['0', '0', '0', '0']
         self.cursor_pos = 0
 
         self.letter_w = 3
         self.letter_h = 5
-        self.addr_x, self.addr_y = self.get_addr_pos()
-        self.addr_pd = 12
-
-        self.addrs: Iterable[(str, int)] = []
-        self.try_to_send_addr_acceptance = False
-        self.should_close_addrs_listening = False
-        t = threading.Thread(target=self.get_new_addrs)
-        t.start()
+        self.letter_pd = 1
+        self.x, self.y = self.get_addr_pos()
 
     def draw(self):
-        px.cls(config.BACKGROUND_COL)
-        super().draw()
+        self.draw_addr_classes()
+        if utils.flicker(0.7):
+            self.draw_cursor()
+
+    def update(self):
+        if (action.move_player_left() or
+            (px.btn(px.KEY_LSHIFT) or px.btn(px.KEY_RSHIFT)) and
+            px.btnp(px.KEY_TAB)):
+            self.cursor_pos -=1
+            if self.cursor_pos < 0:
+                self.cursor_pos = len(self.classes) - 1
+        elif action.move_player_right() or px.btnp(px.KEY_TAB):
+            self.cursor_pos += 1
+            if self.cursor_pos >= len(self.classes):
+                self.cursor_pos = 0
+        if px.btnp(px.KEY_BACKSPACE):
+            self.del_elem_in_addr_class()
+        for digit in range(10):
+            key = px.KEY_0 + digit
+            if px.btnp(key):
+                self.edit_addr_class(chr(key))
+                self.switch_addr_class()
+
+    def draw_addr_classes(self):
+        x = self.x
+        y = self.y
+        for i, c in enumerate(self.classes):
+            empty_digits = 3 - len(c)
+            x += empty_digits * (self.letter_w + self.letter_pd)
+            if len(c):
+                col = config.TEXT2_COL if i == self.cursor_pos else config.TEXT1_COL
+                px.text(x, y, c, col)
+            x += len(c) * (self.letter_w + self.letter_pd)
+            px.text(x, y, '.', config.TEXT2_COL)
+            x += self.letter_pd + 2
+
+    def draw_cursor(self):
+        px.rect(
+            self.x + self.cursor_pos * ((self.letter_w + 1) * 3 - 1 + 4),
+            self.y + self.letter_h + 2,
+            (self.letter_w + self.letter_pd) * 3 - self.letter_pd,
+            1,
+            config.TEXT2_COL)
+
+    def get_addr_pos(self) -> (int, int):
+        w = (self.letter_w + 1) * len('192.168.101.101') - 1
+        return utils.centerize_rect_in_rect(
+            w, self.letter_h,
+            0, 0,
+            config.WINDOW_WDT, config.WINDOW_HGT)
+
+    def edit_addr_class(self, key: str):
+        # current class
+        cc = self.classes[self.cursor_pos]
+        if cc == '0' or len(cc) == 3:
+            cc = key
+        else:
+            cc += key
+        if int(cc) > 255:
+            cc = '255'
+        self.classes[self.cursor_pos] = cc
+
+    def switch_addr_class(self):
+        cc = self.classes[self.cursor_pos]
+        last_class_elem = self.cursor_pos == len(self.classes) - 1
+        if (len(cc) == 3 or cc == '0') and not last_class_elem:
+            self.cursor_pos += 1
+
+    def del_elem_in_addr_class(self):
+        cc = self.classes[self.cursor_pos]
+        if cc == '0':
+            return
+        if len(cc) == 1:
+            cc = '0'
+        else:
+            cc = cc[:-1]
+        self.classes[self.cursor_pos] = cc
+
+
+class ChooseAddr:
+    def __init__(self):
+        self.cursor_pos = 0
+        self.letter_w = 3
+        self.letter_h = 5
+        self.x, self.y = self.get_addr_pos()
+        self.pd = 12
+
+        self.addrs: Iterable[(str, int)] = []
+
+    def draw(self):
         if self.addrs:
             self.draw_addrs()
         else:
-            px.text(self.addr_x, self.addr_y, 'waiting', config.TEXT2_COL if utils.flicker(0.7) else config.TEXT1_COL)
+            col = config.TEXT2_COL if utils.flicker(0.7) else config.TEXT1_COL
+            px.text(self.x, self.y, 'waiting', col)
 
     def update(self):
         if action.move_player_up():
@@ -48,51 +130,9 @@ class Server(PopupMessage):
             self.cursor_pos += 1
             if self.cursor_pos >= len(self.addrs):
                 self.cursor_pos = len(self.addrs) - 1
-        if px.btnp(px.KEY_ESCAPE):
-            self._scenes.pop()
-        if action.resume() and self.addrs:
-            self.try_to_send_addr_acceptance = True
-
-        if self.try_to_send_addr_acceptance:
-            self.try_to_send_addr_acceptance = False
-            t = threading.Thread(target=self.send_addr_acceptance)
-            t.start()
-
-    def draw_addrs(self):
-        px.text(self.addr_x, self.addr_y, self.addrs[self.cursor_pos][0], config.TEXT2_COL)
-        if self.cursor_pos > 0:
-            px.text(
-                self.addr_x + self.addr_pd // 2,
-                self.addr_y - self.addr_pd, self.addrs[self.cursor_pos-1][0], config.TEXT1_COL)
-        if self.cursor_pos < len(self.addrs) - 1:
-            px.text(
-                self.addr_x + self.addr_pd // 2,
-                self.addr_y + self.addr_pd, self.addrs[self.cursor_pos+1][0], config.TEXT1_COL)
-        if utils.flicker(0.7):
-            utils.draw_selection_cursor(
-                self.addr_x - 8,
-                self.addr_y - (self.letter_h - len(config.SELECTION_CURSOR_DATA)) // 2,
-                1)
-
-    def get_new_addrs(self):
-        while True:
-            if self.should_close_addrs_listening:
-                return
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.bind((config.HOST, config.PORT))
-                s.listen(1)
-                s.settimeout(config.SOCKET_TIMEOUT)
-                try:
-                    conn, addr = s.accept()
-                    to_add = True
-                    for i in self.addrs:
-                        if addr[0] in i[0]:
-                            to_add = False
-                            break
-                    if to_add:
-                        self.addrs.append(addr)
-                except TimeoutError:
-                    pass
+    @property
+    def current(self):
+        return self.addrs[self.cursor_pos]
 
     def get_addr_pos(self) -> (int, int):
         w = (self.letter_w + 1) * len('192.168.101.101') - 1
@@ -101,16 +141,98 @@ class Server(PopupMessage):
             0, 0,
             config.WINDOW_WDT, config.WINDOW_HGT)
 
+    def draw_addrs(self):
+        px.text(self.x, self.y, self.addrs[self.cursor_pos][0], config.TEXT2_COL)
+        if self.cursor_pos > 0:
+            px.text(
+                self.x + self.pd // 2,
+                self.y - self.pd, self.addrs[self.cursor_pos-1][0], config.TEXT1_COL)
+        if self.cursor_pos < len(self.addrs) - 1:
+            px.text(
+                self.x + self.pd // 2,
+                self.y + self.pd, self.addrs[self.cursor_pos+1][0], config.TEXT1_COL)
+        if utils.flicker(0.7):
+            utils.draw_selection_cursor(
+                self.x - 8,
+                self.y - (self.letter_h - len(config.SELECTION_CURSOR_DATA)) // 2,
+                1)
+
+class Server(PopupMessage):
+    def __init__(self, scenes: deque):
+        super().__init__(scenes, 'Waiting user')
+        self.y = 12
+
+        self.host: str | None = None
+        self.fill_addr = FillAddr()
+        self.addr_filled = False
+
+        self.choose_addr = ChooseAddr()
+
+        self.try_to_send_addr_acceptance = False
+        self.should_close_addrs_listening = False
+
+    def draw(self):
+        px.cls(config.BACKGROUND_COL)
+        super().draw()
+        if self.addr_filled:
+            self.choose_addr.draw()
+        else:
+            self.fill_addr.draw()
+                    
+    def update(self):
+        if px.btnp(px.KEY_ESCAPE):
+            self._scenes.pop()
+
+        if self.addr_filled:
+            self.choose_addr.update()
+
+            if action.resume() and self.choose_addr.addrs:
+                self.try_to_send_addr_acceptance = True
+
+            if self.try_to_send_addr_acceptance:
+                self.try_to_send_addr_acceptance = False
+                t = threading.Thread(target=self.send_addr_acceptance)
+                t.start()
+        else:
+            self.fill_addr.update()
+            if action.resume():
+                self.addr_filled = True
+                self.host = '.'.join(self.fill_addr.classes)
+                t = threading.Thread(target=self.get_new_addrs)
+                t.start()
+
+    def get_new_addrs(self):
+        while True:
+            if self.should_close_addrs_listening:
+                return
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                s.bind((self.host, config.PORT))
+                s.listen(1)
+                s.settimeout(config.SOCKET_TIMEOUT)
+                try:
+                    conn, addr = s.accept()
+                    to_add = True
+                    for i in self.choose_addr.addrs:
+                        if addr[0] in i[0]:
+                            to_add = False
+                            break
+                    if to_add:
+                        self.choose_addr.addrs.append(addr)
+                except OSError:
+                    pass
+                except TimeoutError:
+                    pass
+
     def send_addr_acceptance(self):
         self.should_close_addrs_listening = True
         while True:
             try:
                 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                    s.bind((config.HOST, config.PORT))
+                    s.bind((self.host, config.PORT))
                     s.listen(1)
                     s.settimeout(config.SOCKET_TIMEOUT)
                     conn, addr = s.accept()
-                    if addr[0] == self.addrs[self.cursor_pos][0]:
+                    if addr[0] == self.choose_addr.current[0]:
                         conn.sendall(b'accept')
                         self.run_game()
             except OSError:
@@ -126,13 +248,7 @@ class Client(PopupMessage):
     def __init__(self, scenes: deque):
         super().__init__(scenes, 'Connect2server')
         self.y = 12
-        self.addr_classes = ['0', '0', '0', '0']
-        self.cursor_pos = 0
-
-        self.letter_w = 3
-        self.letter_h = 5
-        self.letter_pd = 1
-        self.addr_x, self.addr_y = self.get_addr_pos()
+        self.addr = FillAddr()
 
         self.should_send_requests_for_connection = False
         self.was_accept = False
@@ -142,30 +258,12 @@ class Client(PopupMessage):
     def draw(self):
         px.cls(config.BACKGROUND_COL)
         super().draw()
-        self.draw_addr_classes()
-        if utils.flicker(0.7):
-            self.draw_cursor()
+        self.addr.draw()
 
     def update(self):
         if px.btnp(px.KEY_ESCAPE):
             self._scenes.pop()
-        if (action.move_player_left() or
-            (px.btn(px.KEY_LSHIFT) or px.btn(px.KEY_RSHIFT)) and
-            px.btnp(px.KEY_TAB)):
-            self.cursor_pos -=1
-            if self.cursor_pos < 0:
-                self.cursor_pos = len(self.addr_classes) - 1
-        elif action.move_player_right() or px.btnp(px.KEY_TAB):
-            self.cursor_pos += 1
-            if self.cursor_pos >= len(self.addr_classes):
-                self.cursor_pos = 0
-        if px.btnp(px.KEY_BACKSPACE):
-            self.del_elem_in_addr_class()
-        for digit in range(10):
-            key = px.KEY_0 + digit
-            if px.btnp(key):
-                self.edit_addr_class(chr(key))
-                self.switch_addr_class()
+        self.addr.update()
         if action.resume():
             self.should_send_requests_for_connection = True
 
@@ -179,7 +277,7 @@ class Client(PopupMessage):
             try:
                 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     s.settimeout(config.SOCKET_TIMEOUT)
-                    s.connect(('.'.join(self.addr_classes), config.PORT))
+                    s.connect(('.'.join(self.addr.classes), config.PORT))
                     s.sendall(b'')
                     data = s.recv(1024).decode()
                     if data == 'accept':
@@ -194,63 +292,8 @@ class Client(PopupMessage):
             except ConnectionRefusedError:
                 pass
 
-    def edit_addr_class(self, key: str):
-        # current class
-        cc = self.addr_classes[self.cursor_pos]
-        if cc == '0' or len(cc) == 3:
-            cc = key
-        else:
-            cc += key
-        if int(cc) > 255:
-            cc = '255'
-        self.addr_classes[self.cursor_pos] = cc
-
-    def switch_addr_class(self):
-        cc = self.addr_classes[self.cursor_pos]
-        last_class_elem = self.cursor_pos == len(self.addr_classes) - 1
-        if len(cc) == 3 or cc == '0' and not last_class_elem:
-            self.cursor_pos += 1
-
-    def del_elem_in_addr_class(self):
-        cc = self.addr_classes[self.cursor_pos]
-        if cc == '0':
-            return
-        if len(cc) == 1:
-            cc = '0'
-        else:
-            cc = cc[:-1]
-        self.addr_classes[self.cursor_pos] = cc
-
-    def get_addr_pos(self) -> (int, int):
-        w = (self.letter_w + 1) * len('192.168.101.101') - 1
-        return utils.centerize_rect_in_rect(
-            w, self.letter_h,
-            0, 0,
-            config.WINDOW_WDT, config.WINDOW_HGT)
-
     def run_game(self):
         print('game is running')
-
-    def draw_cursor(self):
-        px.rect(
-            self.addr_x + self.cursor_pos * ((self.letter_w + 1) * 3 - 1 + 4),
-            self.addr_y + self.letter_h + 2,
-            (self.letter_w + self.letter_pd) * 3 - self.letter_pd,
-            1,
-            config.TEXT2_COL)
-
-    def draw_addr_classes(self):
-        x = self.addr_x
-        y = self.addr_y
-        for i, c in enumerate(self.addr_classes):
-            empty_digits = 3 - len(c)
-            x += empty_digits * (self.letter_w + self.letter_pd)
-            if len(c):
-                col = config.TEXT2_COL if i == self.cursor_pos else config.TEXT1_COL
-                px.text(x, y, c, col)
-            x += len(c) * (self.letter_w + self.letter_pd)
-            px.text(x, y, '.', config.TEXT2_COL)
-            x += self.letter_pd + 2
 
 
 class OnlineCoopMenu(PopupMessage):
